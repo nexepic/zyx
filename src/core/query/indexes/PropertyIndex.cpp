58483c9922223a98a952c3737441d43f81ec492d
nexepic <remarkable.yh@gmail.com>
2025-05-08 15:47:44 +0800

    ci

--- src/core/query/indexes/PropertyIndex.cpp
+++ src/core/query/indexes/PropertyIndex.cpp
@@ -10,225 +10,234 @@
 
 #include "graph/core/query/indexes/PropertyIndex.h"
 #include <algorithm>
+#include <mutex>
 
 namespace graph::query::indexes {
 
-PropertyIndex::PropertyIndex() = default;
+	PropertyIndex::PropertyIndex() = default;
 
-void PropertyIndex::addProperty(uint64_t nodeId, const std::string& key, const PropertyValue& value) {
-    std::unique_lock lock(mutex_);
+	void PropertyIndex::addProperty(uint64_t nodeId, const std::string &key, const PropertyValue &value) {
+		std::unique_lock lock(mutex_);
 
-    // Remove any existing property with this key for this node
-    removeProperty(nodeId, key);
+		// Remove any existing property with this key for this node
+		removeProperty(nodeId, key);
 
-    // Store in the appropriate index based on the value type
-    std::visit([&](const auto& v) {
-        using T = std::decay_t<decltype(v)>;
+		// Store in the appropriate index based on the value type
+		std::visit(
+				[&](const auto &v) {
+					using T = std::decay_t<decltype(v)>;
 
-        if constexpr (std::is_same_v<T, std::string>) {
-            stringIndex_[key][v].push_back(nodeId);
-        } else if constexpr (std::is_same_v<T, int64_t>) {
-            intIndex_[key][v].push_back(nodeId);
-            // Also add to range index for numeric values
-            double numericValue = static_cast<double>(v);
-            rangeIndex_[key][numericValue].push_back(nodeId);
-        } else if constexpr (std::is_same_v<T, double>) {
-            doubleIndex_[key][v].push_back(nodeId);
-            // Also add to range index
-            rangeIndex_[key][v].push_back(nodeId);
-        } else if constexpr (std::is_same_v<T, bool>) {
-            boolIndex_[key][v].push_back(nodeId);
-        }
-    }, value);
+					if constexpr (std::is_same_v<T, std::string>) {
+						stringIndex_[key][v].push_back(nodeId);
+					} else if constexpr (std::is_same_v<T, int64_t>) {
+						intIndex_[key][v].push_back(nodeId);
+						// Also add to range index for numeric values
+						double numericValue = static_cast<double>(v);
+						rangeIndex_[key][numericValue].push_back(nodeId);
+					} else if constexpr (std::is_same_v<T, double>) {
+						doubleIndex_[key][v].push_back(nodeId);
+						// Also add to range index
+						rangeIndex_[key][v].push_back(nodeId);
+					} else if constexpr (std::is_same_v<T, bool>) {
+						boolIndex_[key][v].push_back(nodeId);
+					}
+				},
+				value);
 
-    // Update reverse mapping
-    nodeProperties_[nodeId][key] = value;
-}
+		// Update reverse mapping
+		nodeProperties_[nodeId][key] = value;
+	}
 
-void PropertyIndex::removeProperty(uint64_t nodeId, const std::string& key) {
-    // Note: mutex is expected to be locked by caller
+	void PropertyIndex::removeProperty(uint64_t nodeId, const std::string &key) {
+		// Note: mutex is expected to be locked by caller
 
-    // Check if the node has this property
-    auto nodeIt = nodeProperties_.find(nodeId);
-    if (nodeIt == nodeProperties_.end()) {
-        return;
-    }
+		// Check if the node has this property
+		auto nodeIt = nodeProperties_.find(nodeId);
+		if (nodeIt == nodeProperties_.end()) {
+			return;
+		}
 
-    auto propertyIt = nodeIt->second.find(key);
-    if (propertyIt == nodeIt->second.end()) {
-        return;
-    }
+		auto propertyIt = nodeIt->second.find(key);
+		if (propertyIt == nodeIt->second.end()) {
+			return;
+		}
 
-    // Remove from the appropriate index based on value type
-    std::visit([&](const auto& v) {
-        using T = std::decay_t<decltype(v)>;
+		// Remove from the appropriate index based on value type
+		std::visit(
+				[&](const auto &v) {
+					using T = std::decay_t<decltype(v)>;
 
-        if constexpr (std::is_same_v<T, std::string>) {
-            auto& nodeIds = stringIndex_[key][v];
-            nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
-            if (nodeIds.empty()) {
-                stringIndex_[key].erase(v);
-                if (stringIndex_[key].empty()) {
-                    stringIndex_.erase(key);
-                }
-            }
-        } else if constexpr (std::is_same_v<T, int64_t>) {
-            // Remove from int index
-            auto& nodeIds = intIndex_[key][v];
-            nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
-            if (nodeIds.empty()) {
-                intIndex_[key].erase(v);
-                if (intIndex_[key].empty()) {
-                    intIndex_.erase(key);
-                }
-            }
+					if constexpr (std::is_same_v<T, std::string>) {
+						auto &nodeIds = stringIndex_[key][v];
+						nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
+						if (nodeIds.empty()) {
+							stringIndex_[key].erase(v);
+							if (stringIndex_[key].empty()) {
+								stringIndex_.erase(key);
+							}
+						}
+					} else if constexpr (std::is_same_v<T, int64_t>) {
+						// Remove from int index
+						auto &nodeIds = intIndex_[key][v];
+						nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
+						if (nodeIds.empty()) {
+							intIndex_[key].erase(v);
+							if (intIndex_[key].empty()) {
+								intIndex_.erase(key);
+							}
+						}
 
-            // Also remove from range index
-            double numericValue = static_cast<double>(v);
-            auto& rangeNodeIds = rangeIndex_[key][numericValue];
-            rangeNodeIds.erase(std::remove(rangeNodeIds.begin(), rangeNodeIds.end(), nodeId), rangeNodeIds.end());
-            if (rangeNodeIds.empty()) {
-                rangeIndex_[key].erase(numericValue);
-                if (rangeIndex_[key].empty()) {
-                    rangeIndex_.erase(key);
-                }
-            }
-        } else if constexpr (std::is_same_v<T, double>) {
-            // Remove from double index
-            auto& nodeIds = doubleIndex_[key][v];
-            nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
-            if (nodeIds.empty()) {
-                doubleIndex_[key].erase(v);
-                if (doubleIndex_[key].empty()) {
-                    doubleIndex_.erase(key);
-                }
-            }
+						// Also remove from range index
+						double numericValue = static_cast<double>(v);
+						auto &rangeNodeIds = rangeIndex_[key][numericValue];
+						rangeNodeIds.erase(std::remove(rangeNodeIds.begin(), rangeNodeIds.end(), nodeId),
+										   rangeNodeIds.end());
+						if (rangeNodeIds.empty()) {
+							rangeIndex_[key].erase(numericValue);
+							if (rangeIndex_[key].empty()) {
+								rangeIndex_.erase(key);
+							}
+						}
+					} else if constexpr (std::is_same_v<T, double>) {
+						// Remove from double index
+						auto &nodeIds = doubleIndex_[key][v];
+						nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
+						if (nodeIds.empty()) {
+							doubleIndex_[key].erase(v);
+							if (doubleIndex_[key].empty()) {
+								doubleIndex_.erase(key);
+							}
+						}
 
-            // Also remove from range index
-            auto& rangeNodeIds = rangeIndex_[key][v];
-            rangeNodeIds.erase(std::remove(rangeNodeIds.begin(), rangeNodeIds.end(), nodeId), rangeNodeIds.end());
-            if (rangeNodeIds.empty()) {
-                rangeIndex_[key].erase(v);
-                if (rangeIndex_[key].empty()) {
-                    rangeIndex_.erase(key);
-                }
-            }
-        } else if constexpr (std::is_same_v<T, bool>) {
-            auto& nodeIds = boolIndex_[key][v];
-            nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
-            if (nodeIds.empty()) {
-                boolIndex_[key].erase(v);
-                if (boolIndex_[key].empty()) {
-                    boolIndex_.erase(key);
-                }
-            }
-        }
-    }, propertyIt->second);
+						// Also remove from range index
+						auto &rangeNodeIds = rangeIndex_[key][v];
+						rangeNodeIds.erase(std::remove(rangeNodeIds.begin(), rangeNodeIds.end(), nodeId),
+										   rangeNodeIds.end());
+						if (rangeNodeIds.empty()) {
+							rangeIndex_[key].erase(v);
+							if (rangeIndex_[key].empty()) {
+								rangeIndex_.erase(key);
+							}
+						}
+					} else if constexpr (std::is_same_v<T, bool>) {
+						auto &nodeIds = boolIndex_[key][v];
+						nodeIds.erase(std::remove(nodeIds.begin(), nodeIds.end(), nodeId), nodeIds.end());
+						if (nodeIds.empty()) {
+							boolIndex_[key].erase(v);
+							if (boolIndex_[key].empty()) {
+								boolIndex_.erase(key);
+							}
+						}
+					}
+				},
+				propertyIt->second);
 
-    // Remove from reverse mapping
-    nodeIt->second.erase(key);
-    if (nodeIt->second.empty()) {
-        nodeProperties_.erase(nodeId);
-    }
-}
+		// Remove from reverse mapping
+		nodeIt->second.erase(key);
+		if (nodeIt->second.empty()) {
+			nodeProperties_.erase(nodeId);
+		}
+	}
 
-std::vector<int64_t> PropertyIndex::findExactMatch(const std::string& key, const PropertyValue& value) const {
-    std::shared_lock lock(mutex_);
+	std::vector<int64_t> PropertyIndex::findExactMatch(const std::string &key, const PropertyValue &value) const {
+		std::shared_lock lock(mutex_);
 
-    return std::visit([&](const auto& v) -> std::vector<int64_t> {
-        using T = std::decay_t<decltype(v)>;
+		return std::visit(
+				[&](const auto &v) -> std::vector<int64_t> {
+					using T = std::decay_t<decltype(v)>;
 
-        if constexpr (std::is_same_v<T, std::string>) {
-            auto indexIt = stringIndex_.find(key);
-            if (indexIt == stringIndex_.end()) {
-                return {};
-            }
+					if constexpr (std::is_same_v<T, std::string>) {
+						auto indexIt = stringIndex_.find(key);
+						if (indexIt == stringIndex_.end()) {
+							return {};
+						}
 
-            auto valueIt = indexIt->second.find(v);
-            if (valueIt == indexIt->second.end()) {
-                return {};
-            }
+						auto valueIt = indexIt->second.find(v);
+						if (valueIt == indexIt->second.end()) {
+							return {};
+						}
 
-            return valueIt->second;
-        } else if constexpr (std::is_same_v<T, int64_t>) {
-            auto indexIt = intIndex_.find(key);
-            if (indexIt == intIndex_.end()) {
-                return {};
-            }
+						return valueIt->second;
+					} else if constexpr (std::is_same_v<T, int64_t>) {
+						auto indexIt = intIndex_.find(key);
+						if (indexIt == intIndex_.end()) {
+							return {};
+						}
 
-            auto valueIt = indexIt->second.find(v);
-            if (valueIt == indexIt->second.end()) {
-                return {};
-            }
+						auto valueIt = indexIt->second.find(v);
+						if (valueIt == indexIt->second.end()) {
+							return {};
+						}
 
-            return valueIt->second;
-        } else if constexpr (std::is_same_v<T, double>) {
-            auto indexIt = doubleIndex_.find(key);
-            if (indexIt == doubleIndex_.end()) {
-                return {};
-            }
+						return valueIt->second;
+					} else if constexpr (std::is_same_v<T, double>) {
+						auto indexIt = doubleIndex_.find(key);
+						if (indexIt == doubleIndex_.end()) {
+							return {};
+						}
 
-            auto valueIt = indexIt->second.find(v);
-            if (valueIt == indexIt->second.end()) {
-                return {};
-            }
+						auto valueIt = indexIt->second.find(v);
+						if (valueIt == indexIt->second.end()) {
+							return {};
+						}
 
-            return valueIt->second;
-        } else if constexpr (std::is_same_v<T, bool>) {
-            auto indexIt = boolIndex_.find(key);
-            if (indexIt == boolIndex_.end()) {
-                return {};
-            }
+						return valueIt->second;
+					} else if constexpr (std::is_same_v<T, bool>) {
+						auto indexIt = boolIndex_.find(key);
+						if (indexIt == boolIndex_.end()) {
+							return {};
+						}
 
-            auto valueIt = indexIt->second.find(v);
-            if (valueIt == indexIt->second.end()) {
-                return {};
-            }
+						auto valueIt = indexIt->second.find(v);
+						if (valueIt == indexIt->second.end()) {
+							return {};
+						}
 
-            return valueIt->second;
-        } else {
-            // Unsupported type
-            return {};
-        }
-    }, value);
-}
+						return valueIt->second;
+					} else {
+						// Unsupported type
+						return {};
+					}
+				},
+				value);
+	}
 
-std::vector<int64_t> PropertyIndex::findRange(const std::string& key, double minValue, double maxValue) const {
-    std::shared_lock lock(mutex_);
+	std::vector<int64_t> PropertyIndex::findRange(const std::string &key, double minValue, double maxValue) const {
+		std::shared_lock lock(mutex_);
 
-    auto rangeIt = rangeIndex_.find(key);
-    if (rangeIt == rangeIndex_.end()) {
-        return {};
-    }
+		auto rangeIt = rangeIndex_.find(key);
+		if (rangeIt == rangeIndex_.end()) {
+			return {};
+		}
 
-    std::vector<int64_t> result;
+		std::vector<int64_t> result;
 
-    // Find all values in the range
-    auto startIt = rangeIt->second.lower_bound(minValue);
-    auto endIt = rangeIt->second.upper_bound(maxValue);
+		// Find all values in the range
+		auto startIt = rangeIt->second.lower_bound(minValue);
+		auto endIt = rangeIt->second.upper_bound(maxValue);
 
-    // Collect all node IDs
-    for (auto it = startIt; it != endIt; ++it) {
-        const auto& nodeIds = it->second;
-        result.insert(result.end(), nodeIds.begin(), nodeIds.end());
-    }
+		// Collect all node IDs
+		for (auto it = startIt; it != endIt; ++it) {
+			const auto &nodeIds = it->second;
+			result.insert(result.end(), nodeIds.begin(), nodeIds.end());
+		}
 
-    // Remove duplicates if any
-    std::sort(result.begin(), result.end());
-    result.erase(std::unique(result.begin(), result.end()), result.end());
+		// Remove duplicates if any
+		std::sort(result.begin(), result.end());
+		result.erase(std::unique(result.begin(), result.end()), result.end());
 
-    return result;
-}
+		return result;
+	}
 
-void PropertyIndex::clear() {
-    std::unique_lock lock(mutex_);
+	void PropertyIndex::clear() {
+		std::unique_lock lock(mutex_);
 
-    stringIndex_.clear();
-    intIndex_.clear();
-    doubleIndex_.clear();
-    boolIndex_.clear();
-    rangeIndex_.clear();
-    nodeProperties_.clear();
-}
+		stringIndex_.clear();
+		intIndex_.clear();
+		doubleIndex_.clear();
+		boolIndex_.clear();
+		rangeIndex_.clear();
+		nodeProperties_.clear();
+	}
 
 } // namespace graph::query::indexes
--- src/core/query/indexes/RelationshipIndex.cpp
+++ src/core/query/indexes/RelationshipIndex.cpp
@@ -9,167 +9,169 @@
  **/
 
 #include "graph/core/query/indexes/RelationshipIndex.h"
+#include <algorithm>
 
 namespace graph::query::indexes {
 
-RelationshipIndex::RelationshipIndex() = default;
+	RelationshipIndex::RelationshipIndex() = default;
 
-void RelationshipIndex::addEdge(int64_t edgeId, int64_t fromNodeId, int64_t toNodeId, const std::string& label) {
-    std::unique_lock lock(mutex_);
+	void RelationshipIndex::addEdge(int64_t edgeId, int64_t fromNodeId, int64_t toNodeId, const std::string &label) {
+		std::unique_lock lock(mutex_);
 
-    // Store edge information
-    EdgeInfo info{fromNodeId, toNodeId, label};
-    edgeInfo_[edgeId] = info;
+		// Store edge information
+		EdgeInfo info{fromNodeId, toNodeId, label};
+		edgeInfo_[edgeId] = info;
 
-    // Add to label index
-    labelToEdges_[label].push_back(edgeId);
+		// Add to label index
+		labelToEdges_[label].push_back(edgeId);
 
-    // Add to node-to-node index
-    nodeToNodeEdges_[fromNodeId][toNodeId].push_back(edgeId);
+		// Add to node-to-node index
+		nodeToNodeEdges_[fromNodeId][toNodeId].push_back(edgeId);
 
-    // Add to outgoing edges index
-    outgoingEdges_[fromNodeId][label].push_back(edgeId);
+		// Add to outgoing edges index
+		outgoingEdges_[fromNodeId][label].push_back(edgeId);
 
-    // Add to incoming edges index
-    incomingEdges_[toNodeId][label].push_back(edgeId);
-}
+		// Add to incoming edges index
+		incomingEdges_[toNodeId][label].push_back(edgeId);
+	}
 
-void RelationshipIndex::removeEdge(int64_t edgeId) {
-    std::unique_lock lock(mutex_);
+	void RelationshipIndex::removeEdge(int64_t edgeId) {
+		std::unique_lock lock(mutex_);
 
-    auto edgeIt = edgeInfo_.find(edgeId);
-    if (edgeIt == edgeInfo_.end()) {
-        return;
-    }
+		auto edgeIt = edgeInfo_.find(edgeId);
+		if (edgeIt == edgeInfo_.end()) {
+			return;
+		}
 
-    const EdgeInfo& info = edgeIt->second;
+		const EdgeInfo &info = edgeIt->second;
 
-    // Remove from label index
-    auto& labelEdges = labelToEdges_[info.label];
-    labelEdges.erase(std::remove(labelEdges.begin(), labelEdges.end(), edgeId), labelEdges.end());
-    if (labelEdges.empty()) {
-        labelToEdges_.erase(info.label);
-    }
+		// Remove from label index
+		auto &labelEdges = labelToEdges_[info.label];
+		labelEdges.erase(std::remove(labelEdges.begin(), labelEdges.end(), edgeId), labelEdges.end());
+		if (labelEdges.empty()) {
+			labelToEdges_.erase(info.label);
+		}
 
-    // Remove from node-to-node index
-    auto& nodeToNodeEdgesList = nodeToNodeEdges_[info.fromNodeId][info.toNodeId];
-    nodeToNodeEdgesList.erase(std::remove(nodeToNodeEdgesList.begin(), nodeToNodeEdgesList.end(), edgeId), nodeToNodeEdgesList.end());
-    if (nodeToNodeEdgesList.empty()) {
-        nodeToNodeEdges_[info.fromNodeId].erase(info.toNodeId);
-        if (nodeToNodeEdges_[info.fromNodeId].empty()) {
-            nodeToNodeEdges_.erase(info.fromNodeId);
-        }
-    }
+		// Remove from node-to-node index
+		auto &nodeToNodeEdgesList = nodeToNodeEdges_[info.fromNodeId][info.toNodeId];
+		nodeToNodeEdgesList.erase(std::remove(nodeToNodeEdgesList.begin(), nodeToNodeEdgesList.end(), edgeId),
+								  nodeToNodeEdgesList.end());
+		if (nodeToNodeEdgesList.empty()) {
+			nodeToNodeEdges_[info.fromNodeId].erase(info.toNodeId);
+			if (nodeToNodeEdges_[info.fromNodeId].empty()) {
+				nodeToNodeEdges_.erase(info.fromNodeId);
+			}
+		}
 
-    // Remove from outgoing edges index
-    auto& outEdges = outgoingEdges_[info.fromNodeId][info.label];
-    outEdges.erase(std::remove(outEdges.begin(), outEdges.end(), edgeId), outEdges.end());
-    if (outEdges.empty()) {
-        outgoingEdges_[info.fromNodeId].erase(info.label);
-        if (outgoingEdges_[info.fromNodeId].empty()) {
-            outgoingEdges_.erase(info.fromNodeId);
-        }
-    }
+		// Remove from outgoing edges index
+		auto &outEdges = outgoingEdges_[info.fromNodeId][info.label];
+		outEdges.erase(std::remove(outEdges.begin(), outEdges.end(), edgeId), outEdges.end());
+		if (outEdges.empty()) {
+			outgoingEdges_[info.fromNodeId].erase(info.label);
+			if (outgoingEdges_[info.fromNodeId].empty()) {
+				outgoingEdges_.erase(info.fromNodeId);
+			}
+		}
 
-    // Remove from incoming edges index
-    auto& inEdges = incomingEdges_[info.toNodeId][info.label];
-    inEdges.erase(std::remove(inEdges.begin(), inEdges.end(), edgeId), inEdges.end());
-    if (inEdges.empty()) {
-        incomingEdges_[info.toNodeId].erase(info.label);
-        if (incomingEdges_[info.toNodeId].empty()) {
-            incomingEdges_.erase(info.toNodeId);
-        }
-    }
+		// Remove from incoming edges index
+		auto &inEdges = incomingEdges_[info.toNodeId][info.label];
+		inEdges.erase(std::remove(inEdges.begin(), inEdges.end(), edgeId), inEdges.end());
+		if (inEdges.empty()) {
+			incomingEdges_[info.toNodeId].erase(info.label);
+			if (incomingEdges_[info.toNodeId].empty()) {
+				incomingEdges_.erase(info.toNodeId);
+			}
+		}
 
-    // Remove edge info
-    edgeInfo_.erase(edgeId);
-}
+		// Remove edge info
+		edgeInfo_.erase(edgeId);
+	}
 
-std::vector<int64_t> RelationshipIndex::findEdgesByLabel(const std::string& label) const {
-    std::shared_lock lock(mutex_);
+	std::vector<int64_t> RelationshipIndex::findEdgesByLabel(const std::string &label) const {
+		std::shared_lock lock(mutex_);
 
-    auto it = labelToEdges_.find(label);
-    if (it == labelToEdges_.end()) {
-        return {};
-    }
+		auto it = labelToEdges_.find(label);
+		if (it == labelToEdges_.end()) {
+			return {};
+		}
 
-    return it->second;
-}
+		return it->second;
+	}
 
-std::vector<int64_t> RelationshipIndex::findEdgesBetweenNodes(int64_t fromNodeId, int64_t toNodeId) const {
-    std::shared_lock lock(mutex_);
+	std::vector<int64_t> RelationshipIndex::findEdgesBetweenNodes(int64_t fromNodeId, int64_t toNodeId) const {
+		std::shared_lock lock(mutex_);
 
-    auto fromIt = nodeToNodeEdges_.find(fromNodeId);
-    if (fromIt == nodeToNodeEdges_.end()) {
-        return {};
-    }
+		auto fromIt = nodeToNodeEdges_.find(fromNodeId);
+		if (fromIt == nodeToNodeEdges_.end()) {
+			return {};
+		}
 
-    auto toIt = fromIt->second.find(toNodeId);
-    if (toIt == fromIt->second.end()) {
-        return {};
-    }
+		auto toIt = fromIt->second.find(toNodeId);
+		if (toIt == fromIt->second.end()) {
+			return {};
+		}
 
-    return toIt->second;
-}
+		return toIt->second;
+	}
 
-std::vector<int64_t> RelationshipIndex::findOutgoingEdges(int64_t nodeId, const std::string& label) const {
-    std::shared_lock lock(mutex_);
+	std::vector<int64_t> RelationshipIndex::findOutgoingEdges(int64_t nodeId, const std::string &label) const {
+		std::shared_lock lock(mutex_);
 
-    auto nodeIt = outgoingEdges_.find(nodeId);
-    if (nodeIt == outgoingEdges_.end()) {
-        return {};
-    }
+		auto nodeIt = outgoingEdges_.find(nodeId);
+		if (nodeIt == outgoingEdges_.end()) {
+			return {};
+		}
 
-    if (label.empty()) {
-        // Return all outgoing edges regardless of label
-        std::vector<int64_t> result;
-        for (const auto& [edgeLabel, edges] : nodeIt->second) {
-            result.insert(result.end(), edges.begin(), edges.end());
-        }
-        return result;
-    } else {
-        // Return edges with specific label
-        auto labelIt = nodeIt->second.find(label);
-        if (labelIt == nodeIt->second.end()) {
-            return {};
-        }
-        return labelIt->second;
-    }
-}
+		if (label.empty()) {
+			// Return all outgoing edges regardless of label
+			std::vector<int64_t> result;
+			for (const auto &[edgeLabel, edges]: nodeIt->second) {
+				result.insert(result.end(), edges.begin(), edges.end());
+			}
+			return result;
+		} else {
+			// Return edges with specific label
+			auto labelIt = nodeIt->second.find(label);
+			if (labelIt == nodeIt->second.end()) {
+				return {};
+			}
+			return labelIt->second;
+		}
+	}
 
-std::vector<int64_t> RelationshipIndex::findIncomingEdges(int64_t nodeId, const std::string& label) const {
-    std::shared_lock lock(mutex_);
+	std::vector<int64_t> RelationshipIndex::findIncomingEdges(int64_t nodeId, const std::string &label) const {
+		std::shared_lock lock(mutex_);
 
-    auto nodeIt = incomingEdges_.find(nodeId);
-    if (nodeIt == incomingEdges_.end()) {
-        return {};
-    }
+		auto nodeIt = incomingEdges_.find(nodeId);
+		if (nodeIt == incomingEdges_.end()) {
+			return {};
+		}
 
-    if (label.empty()) {
-        // Return all incoming edges regardless of label
-        std::vector<int64_t> result;
-        for (const auto& [edgeLabel, edges] : nodeIt->second) {
-            result.insert(result.end(), edges.begin(), edges.end());
-        }
-        return result;
-    } else {
-        // Return edges with specific label
-        auto labelIt = nodeIt->second.find(label);
-        if (labelIt == nodeIt->second.end()) {
-            return {};
-        }
-        return labelIt->second;
-    }
-}
+		if (label.empty()) {
+			// Return all incoming edges regardless of label
+			std::vector<int64_t> result;
+			for (const auto &[edgeLabel, edges]: nodeIt->second) {
+				result.insert(result.end(), edges.begin(), edges.end());
+			}
+			return result;
+		} else {
+			// Return edges with specific label
+			auto labelIt = nodeIt->second.find(label);
+			if (labelIt == nodeIt->second.end()) {
+				return {};
+			}
+			return labelIt->second;
+		}
+	}
 
-void RelationshipIndex::clear() {
-    std::unique_lock lock(mutex_);
+	void RelationshipIndex::clear() {
+		std::unique_lock lock(mutex_);
 
-    labelToEdges_.clear();
-    nodeToNodeEdges_.clear();
-    outgoingEdges_.clear();
-    incomingEdges_.clear();
-    edgeInfo_.clear();
-}
+		labelToEdges_.clear();
+		nodeToNodeEdges_.clear();
+		outgoingEdges_.clear();
+		incomingEdges_.clear();
+		edgeInfo_.clear();
+	}
 
 } // namespace graph::query::indexes
